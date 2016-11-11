91a0f4f0d17259ec98e7f5ad35ec1c7f6fced1e4
MZDisaster <moaazw@gmail.com>
2016-11-11 16:01:43 +0100

    added test decimal model binder

--- Lagarsystem/Global.asax.cs
+++ Lagarsystem/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,9 +14,35 @@ namespace Lagarsystem
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            ModelBinders.Binders.Add(typeof(decimal), new DecimalModelBinder());
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+        }
+
+        public class DecimalModelBinder : IModelBinder
+        {
+            public object BindModel(ControllerContext controllerContext,
+                ModelBindingContext bindingContext)
+            {
+                ValueProviderResult valueResult = bindingContext.ValueProvider
+                    .GetValue(bindingContext.ModelName);
+                ModelState modelState = new ModelState { Value = valueResult };
+                object actualValue = null;
+                try
+                {
+                    actualValue = Convert.ToDecimal(valueResult.AttemptedValue,
+                        CultureInfo.CurrentCulture);
+                }
+                catch (FormatException e)
+                {
+                    modelState.Errors.Add(e);
+                }
+
+                bindingContext.ModelState.Add(bindingContext.ModelName, modelState);
+                return actualValue;
+            }
         }
     }
 }
